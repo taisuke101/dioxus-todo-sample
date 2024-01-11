245fa3682c99bf8ec114b7ca101e7b1a637528e6
taisuke101 <uwattotaitai@gmail.com>
2024-01-11 17:24:35 +0900

    feat: add module file

---
+++ components/src/elements/mod.rs
@@ -0,0 +1,12 @@
+#![allow(non_snake_case)]
+pub mod Button;
+pub mod CardBase;
+pub mod FormInput;
+pub mod Stack;
+pub mod Title;
+
+pub use Button::Button;
+pub use CardBase::CardBase;
+pub use FormInput::FormInput;
+pub use Stack::Stack;
+pub use Title::Title;
