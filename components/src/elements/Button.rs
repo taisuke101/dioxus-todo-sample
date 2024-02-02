044b61ce638cadf7375344a70b482544a061f8c5
taisuke101 <uwattotaitai@gmail.com>
2024-02-02 16:50:59 +0900

    feat: ボタンにPropsを追加

--- components/src/elements/Button.rs
+++ components/src/elements/Button.rs
@@ -3,6 +3,14 @@ use strum::Display;
 
 #[derive(Display)]
 pub enum ButtonType {
+    #[strum(serialize = "button")]
+    Button,
+    #[strum(serialize = "submit")]
+    Submit,
+}
+
+#[derive(Display)]
+pub enum ButtonColor {
     #[strum(serialize = "-primary")]
     Primary,
     #[strum(serialize = "-secondary")]
@@ -15,13 +23,27 @@ pub enum ButtonType {
 pub fn Button<'a>(
     cx: Scope,
     button_type: ButtonType,
+    button_color: ButtonColor,
     title: &'a str,
-    on_click: EventHandler<'a, MouseEvent>,
+    on_click: Option<EventHandler<'a, MouseEvent>>,
+    class: Option<&'a str>,
+    disabled: Option<bool>,
 ) -> Element {
     let button_type = button_type.to_string();
+    let button_color = button_color.to_string();
 
     cx.render(rsx! {
         style { include_str!("./Button.scss") }
-        button { class: format_args!("button-container {}", button_type), onclick: move |event| on_click.call(event), "{title}" }
+        button {
+            r#type: format_args!("{}", button_type),
+            disabled: disabled.unwrap_or(false),
+            class: format_args!("button-container {} {:?}", button_color, class.unwrap_or("")),
+            onclick: move |event| {
+                if let Some(on_click) = &on_click {
+                    on_click.call(event)
+                }
+            },
+            "{title}"
+        }
     })
 }
